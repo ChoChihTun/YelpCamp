a7f9bac0a68bf2c5703eb477a84da34f703602f7
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-05-26 22:17:16 +0800

    Change port listener

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.use(indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, process.env.IP, () => {
   console.log('YelpCamp has started!');
 });
