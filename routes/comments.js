78c12078ccf173e8ddfc8cc8f8bb36137982a7b5
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-05-26 10:51:36 +0800

    Add authorization for edit and delete comment

--- routes/comments.js
+++ routes/comments.js
@@ -44,7 +44,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 // EDIT
-router.get('/:comment_id/edit', (req, res) => {
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
   Comment.findById(req.params.comment_id, (err, foundComment) => {
     if (err) {
       res.redirect('back');
@@ -55,7 +55,7 @@ router.get('/:comment_id/edit', (req, res) => {
 });
 
 // UPDATE
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', checkCommentOwnership, (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err) => {
     if (err) {
       res.redirect('back');
@@ -66,7 +66,7 @@ router.put('/:comment_id', (req, res) => {
 });
 
 // DESTROY
-router.delete('/:comment_id', (req, res) => {
+router.delete('/:comment_id', checkCommentOwnership, (req, res) => {
   Comment.findByIdAndRemove(req.params.comment_id, (err) => {
     if (err) {
       res.redirect('back');
@@ -83,6 +83,25 @@ function isLoggedIn(req, res, next) {
   }
   // if not loggin, go to login page
   return res.redirect('/login');
+}
+
+// Middle ware for authorization
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+      if (err) {
+        res.redirect('back'); // redirect back to where user comes from
+
+        // Checks if user owns comment
+      } else if (foundComment.author.id.equals(req.user._id)) {
+        next();
+      } else {
+        res.redirect('back');
+      }
+    });
+  } else {
+    res.redirect('back');
+  }
 }
 
 module.exports = router;
